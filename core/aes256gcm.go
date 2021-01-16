dd82e03f420e30b2fe8bbfd4e84bd54b95954524
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2021-01-16 21:40:37 +0700

    feat(decrypt): add logic and api

--- core/aes256gcm.go
+++ core/aes256gcm.go
@@ -1,22 +1,38 @@
 package core
  
 import (
+	"errors"
     "crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
 )
  
+// ValidateKeyAndNonce validate length
+func ValidateKeyAndNonce(key, nonce []byte) error{
+	if l := len(key); l != 32 {
+		return errors.New("key length invalid")
+	}
+
+	if l := len(nonce); l != 12 {
+		return errors.New("nonce length invalid")
+	}
+
+	return nil
+}
+
 // Encrypt encrypt function for AES-256-GCM
-func Encrypt(plaintext string, key string, nonce string) (string,error){
+func Encrypt(plaintext, key, nonce string) (string,error){
     bPlaintext := []byte(plaintext)
 
 	bKey, err := base64.StdEncoding.DecodeString(key)
     if err != nil {
 		return "" , err
 	}
-
 	bNonce, err := base64.StdEncoding.DecodeString(nonce)
     if err != nil {
+		return "" , err
+	}
+	if err := ValidateKeyAndNonce(bKey, bNonce); err != nil {
 		return "" , err
 	}
 
@@ -34,4 +50,42 @@ func Encrypt(plaintext string, key string, nonce string) (string,error){
 	ciphertext := base64.StdEncoding.EncodeToString(bCiphertext)
 	
 	return ciphertext , nil
+}
+
+// Decrypt decrypt function for AES-256-GCM
+func Decrypt(ciphertext, key, nonce string) (string,error){
+    bCiphertext, err := base64.StdEncoding.DecodeString(ciphertext)
+    if err != nil {
+		return "" , err
+	}
+
+	bKey, err := base64.StdEncoding.DecodeString(key)
+    if err != nil {
+		return "" , err
+	}
+	bNonce, err := base64.StdEncoding.DecodeString(nonce)
+    if err != nil {
+		return "" , err
+	}
+	if err := ValidateKeyAndNonce(bKey, bNonce); err != nil {
+		return "" , err
+	}
+
+    block, err := aes.NewCipher(bKey)
+    if err != nil {
+      return "" , err
+    }
+  
+    aesgcm, err := cipher.NewGCM(block)
+    if err != nil {
+		return "" , err
+	}
+  
+	bPlainText, err := aesgcm.Open(nil, bNonce, bCiphertext, nil)
+	if err != nil {
+		return "", err
+	}
+	plainText := string(bPlainText)
+	
+	return plainText , nil
 }
--- routers/decrypt.go
+++ routers/decrypt.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/ballpor98/Go-NaCl-S/core"
 )
 
 // DecryptRequestModel encrypt api request model
@@ -20,13 +22,22 @@ type DecryptResponseModel struct {
 
 // Decrypt router
 func Decrypt(c *gin.Context) {
+	//read config
+	secret := c.MustGet("AES_SECRET").(string)
+
+	// validate and parse request
 	var request DecryptRequestModel
 	if err := c.ShouldBindJSON(&request); err != nil {
 	  c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	  return
 	}
 
-	response := DecryptResponseModel{Plaintext: "hello", Nonce: request.Nonce}
+	// handler
+	plaintext, err := core.Decrypt(request.Ciphertext, secret, request.Nonce)
+	if err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
 
+	// response
+	response := DecryptResponseModel{Plaintext: plaintext, Nonce: request.Nonce}
 	c.JSON(http.StatusOK, response)
 }
